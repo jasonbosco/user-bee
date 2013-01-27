7af7a62c69e595d01df1704146ef03dc7c2318f5
Jason Bosco <mail@jasonbos.co>
2013-01-26 22:57:19 -0800

    Framework to process triggers

---
+++ examples/scheduler-add-trigger.js
@@ -0,0 +1,9 @@
+var userBee = require('../lib/user-bee')();
+
+userBee.scheduler.addTrigger({
+    query:"{asd:asd}",
+    frequency:"* * * * * *",
+    emailTemplate:"emailTemplate"
+}, function() {
+    process.exit();
+});
--- examples/scheduler.js
+++ examples/scheduler.js
@@ -1,10 +1,3 @@
 var userBee = require('../lib/user-bee')();
 
-userBee.scheduler.addTrigger({
-    query:"{asd:asd}",
-    frequency:"* * * * *",
-    emailTemplate:"emailTemplate"
-}, function() {
-    process.exit();
-});
-//userBee.scheduler.start();
+userBee.scheduler.start();
