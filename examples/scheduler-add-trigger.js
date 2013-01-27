4dc8a7b01d9ad641189480f25047f4b2980ae6a8
Jason Bosco <mail@jasonbos.co>
2013-01-27 02:00:15 -0800

    Process email trigger, queue email for delivery, mark trigger as triggered. Added worker to send email asynchronously.

--- examples/scheduler-add-trigger.js
+++ examples/scheduler-add-trigger.js
@@ -1,9 +1,13 @@
 var userBee = require('../lib/user-bee')();
 
 userBee.scheduler.addTrigger({
-    query:"{asd:asd}",
-    frequency:"* * * * * *",
-    emailTemplate:"emailTemplate"
-}, function() {
+    query:{userId:123}, // Mongo DB compatible query
+    frequency:"* * * * * *", // First field is for seconds
+    email:{
+        templateName:"welcome-template",
+        subject:"Welcome to UserBee",
+        fromField:"hello@jbos.co"
+    }
+}, function () {
     process.exit();
 });
--- examples/worker.js
+++ examples/worker.js
@@ -1,2 +1,13 @@
-var userBee = require('../lib/user-bee')();
+var path = require('path');
+
+var userBee = require('../lib/user-bee')({
+    emailTemplatesDir:path.resolve(__dirname, 'email-templates'),
+    smtp:{
+        service:"SendGrid",
+        auth:{
+            user:'USERNAME',
+            pass:'PASSWORD'
+        }
+    }
+});
 userBee.workers.start();
