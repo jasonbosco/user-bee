ffcb57ea9fe9de33857ac3cda1d4ccc9af41b98f
Jason Bosco <mail@jasonbos.co>
2013-01-27 19:41:49 -0800

    Refactored interface to increment-attributes

--- examples/increment-attributes.js
+++ examples/increment-attributes.js
@@ -1,8 +1,9 @@
 var userBee = require('../')();
 
-userBee.incrementAttributes(123, [
-    {"Login Count":1}
-], function (err) {
+userBee.incrementAttributes(123, {
+    "Login Count":1,
+    "Doc Count":3
+}, function (err) {
     if (err) console.error(err);
     else console.log("Done");
     process.exit();
